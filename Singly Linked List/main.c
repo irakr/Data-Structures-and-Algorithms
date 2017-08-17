0af1af1ef12e05613032cab4ae759400236b3af4
Irak Rigia <tarak.rigia@gmail.com>
2017-08-18 04:03:51 +0530

    Basic working of singly linked list implemented

--- Singly Linked List/main.c
+++ Singly Linked List/main.c
@@ -8,5 +8,13 @@
 #include "list.h"
 
 int main(int argc, char *argv[]) {
+    S_Head list1;
+    init_head(&list1);
+    
+    push(&list1, (void*)10);
+    push(&list1, (void*)20);
+    push(&list1, (void*)30);
+    push(&list1, (void*)40);
+    
 	return 0;
 }
