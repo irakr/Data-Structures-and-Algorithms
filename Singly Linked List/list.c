9667d4fe3276e5565a7cc0f6d4d0bfeab6a189f8
Irak Rigia <tarak.rigia@gmail.com>
2017-08-18 04:03:51 +0530

    Basic working of singly linked list implemented

--- Singly Linked List/list.c
+++ Singly Linked List/list.c
@@ -4,3 +4,63 @@
  */
 
 #include "list.h"
+#include <stdlib.h>
+#include <string.h>
+
+static inline S_Node *new_node(void *content) {
+    if(!content) {
+        return NULL;
+    }
+    
+    S_Node *node = malloc(sizeof(S_Node));
+    node->content_ = content;
+    node->next_ = NULL;
+    return node;
+}
+
+/* Cleanup resources */
+static inline void cleanup() {
+
+}
+
+void init_head(S_Head *head) {
+    if(!head) {
+        return;
+    }
+    memset(head, '\0', sizeof(*head));
+    
+    atexit(cleanup);    //XXX Try using on_exit() instead
+}
+
+int push(S_Head *head, void *content) {
+    if(!head || !content) {
+        return -1;
+    }
+    
+    S_Node *node = new_node(content);
+    if(!node) {
+        return -1;
+    }
+    
+    // Empty list condition
+    if(!(head->head_))
+        head->head_ = head->tail_ = node;
+    else {  // Filled list condition
+        head->tail_->next_ = node;
+        head->tail_ = node;
+    }
+    
+    return 0;
+}
+
+void print_list(S_Head *head, void *first_content, size_t size) {
+    if(!head || !first_content || (size <= 0)) {
+        return;
+    }
+    
+}
+
+void print_list_cb(void (*printer)(void*)) {
+    if(printer == NULL)
+        return;
+}
--- Singly Linked List/list.h
+++ Singly Linked List/list.h
@@ -6,4 +6,37 @@
 #ifndef S_LINKED_LIST_H
 #define S_LINKED_LIST_H
 
+#include <stdio.h>
+
+/* A singly linked list node data type */
+typedef struct s_node {
+    void *content_;
+    struct s_node *next_;
+} S_Node;
+
+/* Data type for head pointer of a singly linked list */
+typedef struct s_head {
+    S_Node *head_, *tail_; // Optional 'tail' pointer for quick reference to the tail node.
+} S_Head;
+
+
+/* Initialize a list head 
+ * (XXX) For safety, always do this when creating a new S_Head in your program.
+ */
+void init_head(S_Head *head);
+
+/* Create a node with its content_ as the arg-2 and push it to the tail  */
+int push(S_Head*, void*);
+
+/* Traverse through the list linearly while printing the content's ID of each node */
+/*
+ * Arg-1 : List head pointer.
+ * Arg-2 : Pointer to the content in the first node.
+ * Arg-3 : sizeof(content_type)
+ */
+void print_list(S_Head*, void*, size_t); //TODO... This is a little tricky to implement.
+
+/* Similar to print_list() except using a callback function which is capable of printing the contents of that type. */
+void print_list_cb(void (*printer)(void*));
+
 #endif
