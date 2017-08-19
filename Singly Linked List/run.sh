e7ba45ec83a8ee65a068c7ece1a6f718de6b61ec
Irak Rigia <tarak.rigia@gmail.com>
2017-08-19 21:16:32 +0530

    Now, run.py module is implemented

---
+++ Singly Linked List/run.sh
@@ -0,0 +1,8 @@
+echo 'Building Project: Singly Linked List ...'
+make clean
+make 2>/dev/null
+wait
+echo '\t\t(Build successful)\n'
+echo '---------------- Singly Linked List ----------------'
+
+./sllist
